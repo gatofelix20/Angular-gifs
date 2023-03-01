0f2ce2dee3b1e0f6f2448d54a6d25ae6b1544e93
“Jhonny <aniceto.0760@gmail.com>
2023-03-01 05:50:48 -0400

    Se agrego informacion de Quitype y localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { SearchGifsResponse, Gif } from '../interface/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -11,14 +12,18 @@ export class GifsService {
   private _historial: string[] = [];
 
  //Todo cambiar any por su tipo
-  public resultados: any[] = [];
+  public resultados: Gif[] = [];
 
   get historial() {
     return [...this._historial];
   }
 
   constructor( private http: HttpClient) {
+    //this._historial = JSON.parse(localStorage.getItem('historial')!)|| [];
 
+    if( localStorage.getItem('historial') ) {
+      this._historial = JSON.parse (localStorage.getItem('historial') ! );
+    }
   }
 
   //insertar valores
@@ -31,12 +36,16 @@ export class GifsService {
     if( !this._historial.includes( query)) {
       this._historial.unshift( query );
       this._historial = this._historial.splice(0,10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial) );
     }
 
-   this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=Ymh7dFF1VKeRt3jOk8fO2SH5Bkd90ZGK&q=${query} z&limit=10`)
-   .subscribe( (resp: any) =>{
+   this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=Ymh7dFF1VKeRt3jOk8fO2SH5Bkd90ZGK&q=${query} z&limit=10`)
+   .subscribe( (resp) =>{
    console.log(resp.data);
    this.resultados = resp.data;
+
+   
    });
 
   }
